91ffaeff1b4463c155ba7ae167e0ddb17aaa22b5
lkessels <l@lmpkessels.com>
2025-12-04 13:02:11 +0100

    feat: add arithmetic opperations for ALU

---
+++ src/alu/arithmetic.rs
@@ -0,0 +1,17 @@
+/// Arithmetic ops for ALU.
+/// Each functions works on unsigned integer arithmetic on the given arguments.
+pub fn add(x: u32, y: u32) -> u32 {
+    x + y
+}
+
+pub fn sub(x: u32, y: u32) -> u32 {
+    x - y
+}
+
+pub fn mul(x: u32, y: u32) -> u32 {
+    x * y
+}
+
+pub fn div(x: u32, y: u32) -> u32 {
+    x / y
+}
