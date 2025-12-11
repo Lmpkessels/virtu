0372799a44a5f4c3e11305ef772883df3924c687
lkessels <l@lmpkessels.com>
2025-12-11 15:57:57 +0100

    feat: program counter functionallity layers

---
+++ src/pc/program_counter.rs
@@ -0,0 +1,28 @@
+#[derive(Debug)]
+pub struct ProgramCounter {
+    pub value: usize
+}
+
+/// A simple program counter with its functions, to load in Data, Increment data
+/// by +1, check what value is hold within register, and reset to reset register.
+impl ProgramCounter {
+    pub fn new() -> Self {
+        Self { value: 0 }
+    }
+
+    pub fn load(&mut self, addr: usize) {
+        self.value = addr;
+    }
+
+    pub fn increment(&mut self) {
+        self.value += 1;
+    }
+    
+    pub fn value(&self) -> usize {
+        self.value
+    }
+
+    pub fn reset(&mut self) {
+        self.value = 0;
+    }
+}
